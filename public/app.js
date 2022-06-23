cff04648495fa2eb4e11a29d549cdb3204b1f232
kishansripada <kishansripada@gmail.com>
2022-06-22 20:32:37 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -10,13 +10,42 @@ function setCookie(cname, cvalue, exhours) {
 
 btn.onclick = async function () {
     let password = document.getElementById("password").value
-    let isCorrect = await fetch(`${window.origin}/login/${password}`).then(r => r.json())
-    if (isCorrect) {
-        console.log("correct")
+    let datasetUrl = await fetch(`${window.origin}/login/${password}`).then(r => r.json())
+    console.log(datasetUrl.DATASET)
+    if (datasetUrl) {
         setCookie("auth", "XXVeh5U9hVu7Q87rhKQ51g==", 1)
-        console.log(window.origin)
-        window.location.href = `${window.origin}/app`
+   window.location.href = `${window.origin}/app`
+        // change config vars in heroku
+        // fetch("https://api.heroku.com/apps/michigan-cellxgene/config-vars", {
+        //     "method": "PATCH",
+        //     "headers": {
+        //         "Content-Type": "application/json",
+        //         "Accept": "application/vnd.heroku+json; version=3",
+        //         "Authorization": "Bearer 7143941a-cb61-4943-837a-fb0085cfb660"
+        //     },
+        //     "body": {
+        //         "DATASET": datasetUrl.DATASET
+        //     }
+        // })
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/vnd.heroku+json; version=3',
+                Authorization: 'Bearer 7143941a-cb61-4943-837a-fb0085cfb660'
+            },
+            body: `{"DATASET":"https://cellxgenetestbucket1.s3.us-east-2.amazonaws.com/pbmc3k+(1).h5ad"}`
+        };
+
+        fetch('https://api.heroku.com/apps/michigan-cellxgene/config-vars', options).then(r => {
+            // console.log(`loading dataset with url ${r.DATASET}`)
+         
+        }).catch(err => {
+            console.error(err);
+        });
+
+
+
     }
 };
-
 
