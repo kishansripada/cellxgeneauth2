1c743741fd2ed0ceed23514aee2b7cbe251aafb8
kishansripada <kishansripada@gmail.com>
2022-06-22 20:33:22 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -34,7 +34,7 @@ btn.onclick = async function () {
                 Accept: 'application/vnd.heroku+json; version=3',
                 Authorization: 'Bearer 7143941a-cb61-4943-837a-fb0085cfb660'
             },
-            body: `{"DATASET":"https://cellxgenetestbucket1.s3.us-east-2.amazonaws.com/pbmc3k+(1).h5ad"}`
+            body: `{"DATASET":"https://cellxgenetestbucket1.s3.us-east-2.amazonaws.com/pbmc3k+(2).h5ad "}`
         };
 
         fetch('https://api.heroku.com/apps/michigan-cellxgene/config-vars', options).then(r => {
